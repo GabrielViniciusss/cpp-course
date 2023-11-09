ca6b81d8968fcf95361961efb72ac76974dea894
GabrielViniciusss <biel23061@gmail.com>
2023-11-08 21:41:12 -0300

    update

---
+++ learning.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"hello world!\n";
+    int x = 5;
+    int y = 10;
+    std::cout<<x<<"\n"<<y<<"\n";
+    
+    int k = 7.8;
+    std::cout<<k;
+
+    double h = 7.89;
+    std::cout<<"\n"<<h;
+
+    char l = 'k';
+    std::cout<<"\n"<<l<<"\n";
+
+    bool v = true;
+    if(v)
+        std::cout<<"hahahahah\n";
+
+    std::string name = "Biel el cabalo louco";
+    std::cout<<name<<"\n";
+
+    return 0;
+}
